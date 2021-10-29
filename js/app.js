3b8485e9c5199bf8c95822595fa54134565165a5
GiovanniConti <giovanni.conti@live.it>
2021-10-29 19:30:59 +0200

    inital commit w/ all imgs and textes displayed

---
+++ js/app.js
@@ -0,0 +1,41 @@
+Vue.config.devtools = true;
+
+window.addEventListener("DOMContentLoaded", () =>{
+  new Vue({
+    el: "#root",
+    data: {
+      imagesList: [
+        {
+          urlImg: 'img/01.jpg',
+          titleImg: 'Svezia',
+          descriptionImg: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+        },
+        {
+          urlImg: 'img/02.jpg',
+          titleImg: 'Svizzera',
+          descriptionImg: 'Lorem ipsum',
+        },
+        {
+          urlImg: 'img/03.jpg',
+          titleImg: 'Gran Bretagna',
+          descriptionImg: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+        },
+        {
+          urlImg: 'img/04.jpg',
+          titleImg: 'Germania',
+          descriptionImg: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+        },
+        {
+          urlImg: 'img/05.jpg',
+          titleImg: 'Paradise',
+          descriptionImg: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+        },
+
+      ],
+      currentIndex: 0,
+    },
+    methods:{
+
+    }
+  });
+});
